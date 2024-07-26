53d76368c05a87427f0f44378a4b21192189b5ad
Serhii Veretennikov <gridis72@gmail.com>
2024-07-26 12:38:12 +0200

    chore: research commit

--- include/libunit/assert.h
+++ include/libunit/assert.h
@@ -1,6 +1,6 @@
 #ifndef ASSERT_H
 # define ASSERT_H
 
-int	assert_int_equal(int res, int exp);
+int	assert_int_equal(int lable, int res, int exp);
 
 #endif
--- src/asserts/assert_int_equal.c
+++ src/asserts/assert_int_equal.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "libft/color.h"
 
-int	assert_int_equal(int res, int exp)
+int	assert_int_equal(int lable, int res, int exp)
 {
 	if (res == exp)
-		return (printf("[ASSERT_OK]"), 1);
-	printf(CLR CLR_RED_FG CLR_END"[ASSERT_KO]\n" CLR_RESET);
+		return (printf(CLR CLR_GREEN_FG CLR_END "[%d.ASSERT_OK]" CLR_RESET, lable), 1);
+	printf(CLR CLR_RED_FG CLR_END"[%d.ASSERT_KO]\n" CLR_RESET, lable);
 	printf("%d is not equal %d\n", res, exp);
 	return (0);
 }
