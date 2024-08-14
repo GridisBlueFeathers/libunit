0fd0c78f4a8f1c7ffcc130ab8852bbb61e1541c0
Serhii Veretennikov <gridis72@gmail.com>
2024-08-14 16:35:56 +0200

    fix: changed name of int data struct

--- src/assert/int/data_dup.c
+++ src/assert/int/data_dup.c
@@ -1,10 +1,10 @@
 #include "libunit/assert.h"
 
-t_assert_data_int	*assert_int_data_dup(t_assert_data *data)
+t_assert_int_data	*assert_int_data_dup(t_assert_data *data)
 {
-	t_assert_data_int *res;
+	t_assert_int_data *res;
 
-	res = (t_assert_data_int *)calloc(1, sizeof(t_assert_data_int));
+	res = (t_assert_int_data *)calloc(1, sizeof(t_assert_int_data));
 	if (!res)
 		return (NULL);
 	res->exp = *((int *)data->exp);
